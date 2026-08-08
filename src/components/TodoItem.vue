d3c3fe7cb38a9695c53ab5fa545095fd143d1709
Tatiana Kh <ts.khol@gmail.com>
2026-08-08 18:25:41 +0300

    cleanup: use standard Vue templates and CSS

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,15 @@
-<template lang='pug'>
-li.todo-item
-  span.todo-item__name(
-    :class="{ 'todo-item__name_completed': todo.done }"
-    @click="emitToggleTask"
-  ) {{ todo.name }}
-  .todo-item__delete(@click="emitDeleteTask")
+<template>
+  <li class="todo-item">
+    <span
+      class="todo-item__name"
+      :class="{ 'todo-item__name_completed': todo.done }"
+      @click="emitToggleTask"
+    >{{ todo.name }}</span>
+    <div
+      class="todo-item__delete"
+      @click="emitDeleteTask"
+    />
+  </li>
 </template>
 
 <script setup>
@@ -24,33 +29,40 @@ function emitDeleteTask() {
 </script>
 
 <style>
-.todo-item
-  display: flex
-  justify-content: space-between
-  height: 40px
+.todo-item {
+  display: flex;
+  justify-content: space-between;
+  height: 40px;
+}
 
-.todo-item:hover
-  font-size: larger
+.todo-item:hover {
+  font-size: larger;
+}
 
-.todo-item:hover
-  list-style-type: disc
+.todo-item:hover {
+  list-style-type: disc;
+}
 
-.todo-item__name
-  cursor: pointer
+.todo-item__name {
+  cursor: pointer;
+}
 
-.todo-item__name_completed
-  text-decoration: line-through
-  color: #9a8c98
+.todo-item__name_completed {
+  text-decoration: line-through;
+  color: #9a8c98;
+}
 
-.todo-item__delete
-  display: initial
-  width: 30px
-  cursor: pointer
-  text-decoration: none
-  color: #5a0700
-  mask: url("@/assets/icons/close.svg") no-repeat center
-  background: black
+.todo-item__delete {
+  display: initial;
+  width: 30px;
+  cursor: pointer;
+  text-decoration: none;
+  color: #5a0700;
+  mask: url("@/assets/icons/close.svg") no-repeat center;
+  background: black;
+}
 
-.todo-item__delete:hover
-  background: #8F0000
+.todo-item__delete:hover {
+  background: #8F0000;
+}
 </style>
